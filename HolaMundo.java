3f0489f301838b199493fdc417fcccf156149080
Raul Pulido <rpultor0709@g.educaand.es>
2024-10-25 20:33:27 +0200

    Añade la suma de dos números

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
+		    int num1 = 5, num2 = 3;
         System.out.println("Hola mundo");
+        System.out.println("La suma de los números es " + (num1+num2);
     }
 }
